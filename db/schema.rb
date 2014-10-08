8e5451c1e8c8ca3ca85318422dff5f554d7c95b0
Mixolidia Gautreaux <mixolidia@gmail.com>
2014-10-08 13:36:03 -0700

    migration change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141008030034) do
+ActiveRecord::Schema.define(version: 20141008064033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 20141008030034) do
 
   create_table "products", force: true do |t|
     t.integer  "amount"
+    t.integer  "vendor_id"
+    t.integer  "product_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "sales", force: true do |t|
+    t.integer  "amount"
+    t.datetime "p_time"
     t.integer  "vendor_id"
     t.integer  "product_id"
     t.datetime "created_at"
